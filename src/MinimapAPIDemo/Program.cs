f4771f9062138bd59541530e260c6c9cc3279f24
Łukasz Hamera <lucas.hamera@gmail.com>
2021-08-29 15:07:28 +0200

    Added GraphQL

--- src/MinimapAPIDemo/Program.cs
+++ src/MinimapAPIDemo/Program.cs
@@ -3,8 +3,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks();
+builder.Services.AddGraphQLServer();
 
 var app = builder.Build();
 app.MapHealthChecks("/health");
+app.MapGraphQL();
 
 await app.RunAsync();
