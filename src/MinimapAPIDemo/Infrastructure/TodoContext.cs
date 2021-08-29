3b47d3de896df3f404ff62006537b353516dfc7f
Łukasz Hamera <lucas.hamera@gmail.com>
2021-08-29 20:29:30 +0200

    Added DbContext

---
+++ src/MinimapAPIDemo/Infrastructure/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimapAPIDemo.Core;
+
+namespace MinimapAPIDemo.Infrastructure;
+
+public class TodoContext: DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Todo> Todos { get; set; }
+}
