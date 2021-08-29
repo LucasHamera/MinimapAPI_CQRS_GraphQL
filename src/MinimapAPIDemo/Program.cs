4bfd757226e821b4bd139d15246cc823ebc30d87
Łukasz Hamera <lucas.hamera@gmail.com>
2021-08-29 15:03:24 +0200

    Added health endpoint

--- src/MinimapAPIDemo/Program.cs
+++ src/MinimapAPIDemo/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+app.MapHealthChecks("/health");
 
 await app.RunAsync();
