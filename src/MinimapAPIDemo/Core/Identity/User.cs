e1d5a82795a7c26bc32c7400cb7693772f0c58f7
Łukasz Hamera <lucas.hamera@gmail.com>
2021-08-30 18:26:28 +0200

    User model

---
+++ src/MinimapAPIDemo/Core/Identity/User.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace MinimapAPIDemo.Core.Identity;
+
+public class User
+{
+    private User()
+    {
+
+    }
+
+    public User(string login, string password)
+    {
+        Login = login;
+        Password = password;
+    }
+
+    public string Login { get; private set; }
+
+    public string Password { get; private set; }
+}
