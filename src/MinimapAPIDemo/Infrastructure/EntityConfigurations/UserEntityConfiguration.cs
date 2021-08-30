45b92348d7026867bf5a807c14d633ea9dcea3a9
Łukasz Hamera <lucas.hamera@gmail.com>
2021-08-30 18:28:27 +0200

    User entity configuration

---
+++ src/MinimapAPIDemo/Infrastructure/EntityConfigurations/UserEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using MinimapAPIDemo.Core.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MinimapAPIDemo.Infrastructure.EntityConfigurations;
+
+public class UserEntityConfiguration: IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(x => x.Login);
+
+        builder.Property(x => x.Login).HasColumnName("Login");
+        builder.Property(x => x.Password).HasColumnName("Description");
+    }
+}
